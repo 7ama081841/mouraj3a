dfd5ed1b1bc0200f7da49dacb56ac75f4691df12
mohamed123 <chaabenim318@gmail.com>
2023-12-04 22:15:52 +0100

    udate file

--- mourj3a-oop.js
+++ mourj3a-oop.js
@@ -1,96 +1,90 @@
-// Defining Object 
-let user = { // hehta el object 
+// Defining Object
+let user = {
+    // hehta el object
+
     // Properties
     firsName: "mohamed",
     lastName: "chaabeni",
-    adresses: { // nejem na3mel Object f wost Object
-        manouba : "tborba",
-        tounes : "zahrouni"
-    } ,
-    // Methodes
-    getFoullName: function () {
-        return `${user.firsName} ${user.lastName}`
+    adresses: {
+        // nejem na3mel nested Object
+        manouba: "tborba",
+        tounes: "zahrouni",
     },
 
-    getFoullName: () =>  `${user.firsName} ${user.lastName}` // nejem na3malha Arrow function
-    
+    // Methodes
+    getFoullName: function () {
+        return `${user.firsName} ${user.lastName}`;
+    },
 
+    getFoullNameArrow: () => `${this.firsName} ${this.lastName}`, // nejem na3malha Arrow function
 };
 
-// Dot Notation vs Braket Notation 
-let user = {
+// Dot Notation vs Braket Notation
 
-    "name": "mohamed",
-    "age": 32
-
+const myObj = {
+    "one": 1,
+    "two-": 2,
+    1: "one", // temchi 3adi
+    2: "two", // temchi 3adi
+    three-: 3, // ta3tini SyntaxError
 };
 
-// Dot Notation
-console.log(user.name) // hethi el Dot Notation
-console.log(user.name - )  // ta3mali SyntaxError ken n7ot el (-)
+let myVar = "name"
 
-// Braket Notation
-console.log(user["name"]) // hethi el Braket Notation
-console.log(user["age-"])  // t7otli undefined
-// _________________________________________________________________________
-let user = {
-
-    1 : "one", // nejem a3mel el key kima haka
-    2 : "two"
-
+let myName = {
+    name : "mohamed"
 };
 
 // Dot Notation
-console.log(user.1) // ta3mali SyntaxError
+console.log(myObj.one);
+console.log(myObj.two-); // ta3tini SyntaxError
+console.log(myObj.1); // ta3tini SyntaxError
+console.log(myName.myVar) // ta3tini undefined
 
 // Braket Notation
-console.log(user[1]) // tedem normal
-// ______________________________________________________________
-let x = "name";
+console.log(myObj["one"]); // hethi el Braket Notation
+console.log(myObj["two-"]); // el mothode hethi temchi 3adi 
+console.log(myObj["2"]); // temchi 3adi
+console.log(myName[myVar]) // nejem n7ot variable fi3outh el string
+console.log(myName["name"])
 
-let user = {
-    name: "mohamed"
+// _______________________________ new object => New Keyword ___________________________________
+let obj = new Object(); // nejem na3mel el Object haka
+
+obj.firstName = "mohamed"; // nejem na3mel set haka
+
+obj["lastName"] = "chaabeni"; // nejem na3mel set bel Braket Notation
+
+obj.getFulName = () => {
+    // nejem na3mel function lel Objcet haka
+    return `this is full name ${user.firstName} ${user.lastName} `;
 };
 
-// Dot Notation
-console.log(user.x) // t7otli undefined
+// _________________________________ Object.create _____________________________________________
 
-// Braket Notation
-console.log(user[myVariable]) // te5dem normal
-
-// New Keyword
-let user = new Object() // nejem na3mel el Object haka 
-
-user.firstName = "mohamed" // nejem na3mel set haka
-
-user["lastName"] = "chaabeni" // nejem na3mel set bel method hethi
-
-user.getFulName =  () => { // nejem na3mel function lel Objcet haka
-    return `this is full nama ${user.firstName} ${user.lastName} `
-}
-
-// Object.create
 let mainObj = {
-    hasDecount : false , 
-    Message : function () {
+    hasDecount: false,
+    Message: function () {
         return `you${mainObj.hasDecount ? "" : " don't"} hav Decount`; // nejem na3mel condition el mo5tasra
-    }
-}
+    },
+    thisMessage: function () {
+        return `you${this.hasDecount ? "" : " don't"} hav Decount`; // yelzemni n7ot this
+    },
+};
 
-let obj1 = Object.create(mainObj) // nejem na3mel object mn object e5er
+let obj1 = Object.create(mainObj); // nejem na3mel object mn object e5er
 
-obj1.hasDecount = true // w nejem na3malou set value 5asa bih
+obj1.hasDecount = true; // w nejem na3malou set value 5asa bih
 
 // Object.assign
 const target = {
-    prop : "value" 
-}
+    prop: "value",
+};
 
-Object.assign(target, obj1 , or-more, {object : "e5er"} ); // ycopili el les object w y7othomli fel object ell 7ajti bih
+Object.assign(target, obj1, or - more, { object: "e5er" }); // ycopili el les object w y7othomli fel object ell 7ajti bih
 
-Object.assign( {} , target, {object : "e5er"} ); // nejem na3mel object jdid wn7ot fih object el 9dim
+Object.assign({}, target, { object: "e5er" }); // nejem na3mel object jdid wn7ot fih object el 9dim
 
 // Delete Operator
-delete user.name // delete props not object
-delete user["name"] // nejem na3mel bel brackets notation
-
+delete user.name; // delete props not object
+delete user["name"]; // nejem na3mel bel brackets notation
--- test.js
+++ test.js
@@ -1,57 +1,19 @@
-const str1 = "mohamed@chaabeni-ahmed@salhi-rida@melki";
+let mainObj = {
+    hasDecount: false,
+    Message: function () {
+        return `you${mainObj.hasDecount ? "" : " don't"} hav Decount`; // nejem na3mel condition el mo5tasra
+    },
+    thisMessage: function () {
+        return `you${this.hasDecount ? "" : " don't"} hav Decount`; // yelzemni n7ot this
+    },
+};
 
-function FormatToArray(obj) {
-  var arr = [];
+console.log(mainObj.hasDecount);
+console.log(mainObj.thisMessage());
 
-  obj.split("-").map((obj) => {
-    arr.push(obj);
-  });
+let obj1 = Object.create(mainObj); // nejem na3mel object mn object e5er
 
-  return arr.map((el) => {
-    return { name: el.split("@")[0], lastName: el.split("@")[1] };
-  });
-}
+obj1.hasDecount = true; // w nejem na3malou set value 5asa bih
 
-// console.log(FormatToArray(str1))
-
-const list = [
-  { name: "mohamed", note: undefined },
-  { name: "safe", note: 7 },
-  { name: "omar", note: 13 },
-  { name: "sara", note: 18 },
-];
-
-function FormatToArray(tab) {
-  return tab.map((list) => {
-    return list.note < 10
-      ? { name: list.name, note: list.note, mention: "passable" }
-      : { name: list.name, note: list.note, mention: "bien" };
-  });
-}
-
-// console.log(FormatToArray(list))
-
-const users = [
-  { name: "monjiya", comment: "j'aime ce poste <3" },
-  { name: "monya", comment: "le poste est null :( " },
-  { name: "carlos", comment: "salut l'algérie" },
-];
-
-function MoodDetect(list) {
-  return list.map((t) => {
-   
-    if (t.comment.includes("<3")) {
-      return { name: t.name, etat: "heureux" };
-    } else if (t.comment.includes(":(")) {
-      // includes traja3li boolean na3mel beha seach
-      return { name: t.name, etat: "triste" };
-    } else {
-      return { name: t.name, etat: "neutre" };
-    }
-
-  });
-}
-
-console.log(MoodDetect(users));
-
-// console.log(users[0].concat);
+console.log(obj1.hasDecount);
+console.log(obj1.thisMessage());
